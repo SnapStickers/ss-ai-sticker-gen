f288263adae2449933dd7136fe692a02975d551b
SnapStickers <snapstickersteam@gmail.com>
2026-06-23 03:30:33 -0400

    Update upload-artwork.js

--- api/upload-artwork.js
+++ api/upload-artwork.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
     }
 
     const blob = await put(`artwork/${Date.now()}-sticker.png`, buffer, {
-      access: "public",
+      access: "private",
       contentType: "image/png",
       addRandomSuffix: true,
     });
